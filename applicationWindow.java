4bcfd3109b5a40ee2e4d63bdbec60460216c090d
Hammer2233 <emailforclass22@gmail.com>
2025-02-25 17:26:52 -0500

    Update applicationWindow.java
    Added buttons for:
    -Checking username
    -Changing backup path
    -Changing mirthdb path

--- applicationWindow.java
+++ applicationWindow.java
@@ -19,6 +19,7 @@ import javax.swing.GroupLayout;
 import java.awt.Desktop;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -35,6 +36,9 @@ public class applicationWindow extends JFrame implements ActionListener
 
     private JButton archiveChannels;
     private JButton fullMirthExport;
+    private JButton checkUsernameButton;
+    private JButton changeBackupPath;
+    private JButton changeMirthDirPath;
 
     private static JTextArea logTextArea;
     private JScrollPane logTextScroll;
@@ -68,6 +72,19 @@ public class applicationWindow extends JFrame implements ActionListener
         fullMirthExport = new JButton("EXPORT MIRTH CONFIG");
         fullMirthExport.addActionListener(new createFullMirthBackup());
         westPanel.add(fullMirthExport);
+
+        checkUsernameButton = new JButton("CHECK USERNAME");
+        checkUsernameButton.addActionListener(new checkUsername());
+        westPanel.add(checkUsernameButton);
+
+        changeBackupPath = new JButton("CHANGE BACKUP PATH");
+        changeBackupPath.addActionListener(new changeBackupDir());
+        westPanel.add(changeBackupPath);
+
+        changeMirthDirPath = new JButton("CHANGE MIRTH PATH");
+        changeMirthDirPath.addActionListener(new changeMirthDBDir());
+        westPanel.add(changeMirthDirPath);
+
         
         westPanel.setBackground(new java.awt.Color(214, 216, 233));
         westPanel.setPreferredSize(new Dimension(200, 100)); // Preferred size for the panel
@@ -121,11 +138,14 @@ public class applicationWindow extends JFrame implements ActionListener
                 channelExport.includeCodeTemplates("NO");
                 runChannelExport();
             }
+            channelExport.includeCodeTemplates("NO");
         }
     }
 
     public static String runChannelExport()
     {
+        Main.setBackupFolder();
+        channelExport.isFullMirthExportCheck("NO");
         System.out.println("PERFORMING CHANNEL BACKUP");
         String host = Main.returnHost();
         channelExport exportChannels = new channelExport();
@@ -139,8 +159,9 @@ public class applicationWindow extends JFrame implements ActionListener
         {
             e1.printStackTrace();
         }
-        logCommands.returnArchivedChannels("C:\\AntekHW\\CALEBMIRTHTESTING\\FullChannelExport\\channelBackup\\");
+        logCommands.returnArchivedChannels(Main.getBackupFolder()+"\\channelBackup\\");
         setLogWindow();
+        Main.deleteBuildingBlockFiles(); //RE-ENABLE ME: 
         return "exported channels";
     }
 
@@ -149,7 +170,8 @@ public class applicationWindow extends JFrame implements ActionListener
         @Override
         public void actionPerformed(ActionEvent e)
         {
-            System.out.println("PERFORMING FULL MIRTH CONFIGURATION EXPORT");
+            channelExport.clearChannelFolder();
+            Main.setBackupFolder();
             logCommands.exportToLog("PERFORMING FULL MIRTH CONFIGURATION EXPORT");
             channelExport.isFullMirthExportCheck("YES");
             
@@ -181,7 +203,8 @@ public class applicationWindow extends JFrame implements ActionListener
                 // TODO Auto-generated catch block
                 e1.printStackTrace();
             }
-            logCommands.exportToLog("Full configuration exported to: C:\\AntekHW\\CALEBMIRTHTESTING\\FullChannelExport\\fullMirthExport\\fullExport");
+            Main.deleteBuildingBlockFiles(); //RE-ENABLE ME: 
+            logCommands.exportToLog("Full configuration exported to: " + Main.getBackupFolder() +"fullExport");
         }
     }
 
@@ -190,5 +213,86 @@ public class applicationWindow extends JFrame implements ActionListener
         String fullLog = logCommands.exportCurrentLogToWindow();
         logTextArea.setText(fullLog);
         return "";
+    }
+
+    private static class checkUsername implements ActionListener
+    {
+        @Override
+        public void actionPerformed(ActionEvent e)
+        {
+            logCommands.exportToLog("Running Query for current Mirth username");
+            String host = Main.returnHost();
+            String currentUsername = SQLCommand.checkUN(host);
+            logCommands.exportToLog("CURRENT USERNAME: " + currentUsername);
+            JOptionPane.showMessageDialog(labelVersion, "Username is: '" + currentUsername + "'");
+        }
+    }
+
+    private static class changeBackupDir implements ActionListener
+    {
+        @Override
+        public void actionPerformed(ActionEvent e)
+        {
+            logCommands.exportToLog("Changing Mirth backup directory");
+            String newPath = getOverrideDir("Change Mirth Export Backup Destination:");
+            if(newPath == "NO CHANGES MADE")
+            {
+                logCommands.exportToLog("NO CHANGES MADE TO BACKUP DIRECTORY");
+            }
+            else
+            {
+                logCommands.exportToLog("Backup Path Changed: " + newPath);
+                Main.setChangedBackup(newPath);
+            }
+        }
+    }
+
+    public static String getOverrideDir(String Dialog) 
+    {
+      String folderPath;
+      JFileChooser changeFolder = new JFileChooser("C:\\");
+      changeFolder.setDialogTitle(Dialog);
+      changeFolder.setFileSelectionMode(1);
+      changeFolder.setAcceptAllFileFilterUsed(false);
+      if (changeFolder.showOpenDialog(labelVersion) == 0) 
+      {
+        folderPath = changeFolder.getSelectedFile().getAbsolutePath() + "\\";
+        //Main.setChangedBackup(folderPath);
+        return folderPath;
+      } 
+      else 
+      {
+        folderPath = "";
+        return "NO CHANGES MADE";
+      }
+    }
+
+    private static class changeMirthDBDir implements ActionListener
+    {
+        @Override
+        public void actionPerformed(ActionEvent e)
+        {
+            logCommands.exportToLog("Changing Target Mirth database directory");
+            Object[] options = { "CONTINUE", "CANCEL" };
+            int exportCTLsTF = JOptionPane.showOptionDialog(labelVersion, "When choosing the new DB directory\ndo not select the 'mirthdb' folder.", "EXPORT MIRTH CHANNEL", 0, 2, null, options, options[1]);
+            if (exportCTLsTF == 0)
+            {
+                System.out.println("ACKd");
+                String newPath = getOverrideDir("Change target Mirth database Destination:");
+                if(newPath == "NO CHANGES MADE")
+                {
+                    logCommands.exportToLog("NO CHANGES MADE TO TARGET DB DIRECTORY");
+                }
+                else
+                {
+                    logCommands.exportToLog("Target DB Path Changed: " + newPath);
+                    Main.setChangedMirthDB(newPath);
+                }
+            }
+            else if(exportCTLsTF == 1)
+            {
+                System.out.println("CANCELLED");
+            }
+        }
     }
 }
